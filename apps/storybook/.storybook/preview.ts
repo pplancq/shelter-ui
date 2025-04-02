f00b0645e4a62b3c5857dff93f1165a06e5597a5
pplancq <paul.plancq@outlook.fr>
2025-04-02 18:34:01 +0200

    chore(storybook): add accessibility addons, update viewport settings

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,11 +1,20 @@
 import type { Preview } from '@storybook/react';
+import { viewPorts } from './viewPort';
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      viewports: viewPorts,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        order: ['Fondations', 'Components'],
       },
     },
   },
