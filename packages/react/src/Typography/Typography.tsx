05888f1087b05c5fbe3ff1df7bae377481b5c01a
pplancq <paul.plancq@outlook.fr>
2025-04-19 15:12:23 +0200

    feat(react): add Typography component with variants and responsive styles

---
+++ packages/react/src/Typography/Typography.tsx
@@ -0,0 +1,77 @@
+import { clsx } from '@/utils/clsx';
+import type { ExtendableComponent } from '@/utils/types';
+import { type ElementType, type PropsWithChildren, useMemo } from 'react';
+
+type SizeTitle = 1 | 2 | 3 | 4 | 5 | 6;
+type SizeText = 'small' | 'medium' | 'large';
+type TypographyPropsBase<C extends ElementType> = ExtendableComponent<C>;
+type TypographyPropsTitle<C extends ElementType> = TypographyPropsBase<C> & {
+  variant: 'display' | 'heading';
+  size?: SizeTitle;
+  bold?: false;
+};
+type TypographyPropsText<C extends ElementType> = TypographyPropsBase<C> & {
+  variant?: 'text';
+  size?: 'smallest' | 'smaller' | SizeText;
+  bold?: true;
+};
+type TypographyPropsLabel<C extends ElementType> = TypographyPropsBase<C> & {
+  variant: 'label';
+  size?: SizeText;
+  bold?: false;
+};
+type TypographyPropsCode<C extends ElementType> = TypographyPropsBase<C> & {
+  variant: 'code';
+  size?: SizeText;
+  bold?: false;
+};
+
+export type TypographyProps<C extends ElementType> =
+  | TypographyPropsTitle<C>
+  | TypographyPropsText<C>
+  | TypographyPropsLabel<C>
+  | TypographyPropsCode<C>;
+
+export const Typography = <C extends ElementType = 'p'>(props: PropsWithChildren<TypographyProps<C>>) => {
+  const {
+    variant = 'text',
+    size = 'medium',
+    component,
+    className,
+    bold,
+    ...typographyProps
+  } = props as TypographyPropsBase<C>;
+
+  const Component = useMemo(() => {
+    const defaultComponent = {
+      display: 'span',
+      heading: `h${size}` as ElementType,
+      text: 'p',
+      label: 'span',
+      code: 'code',
+    } as Record<string, ElementType>;
+
+    return component || defaultComponent[variant];
+  }, [component, variant, size]);
+
+  const typographyClassName = clsx(
+    'typography',
+    variant === 'display' && `d${size}`,
+    variant === 'heading' && `h${size}`,
+    variant === 'text' && `text${bold ? '-bold' : ''}-${size}`,
+    variant === 'label' && `label-${size}`,
+    variant === 'code' && `code-${size}`,
+    className,
+  );
+
+  return (
+    <Component
+      className={typographyClassName}
+      {...typographyProps}
+      role={variant === 'display' ? 'heading' : typographyProps.role}
+      aria-level={['display', 'heading'].includes(variant) ? size : typographyProps['aria-level']}
+    />
+  );
+};
+
+Typography.displayName = 'Typography';
