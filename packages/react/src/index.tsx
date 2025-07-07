8615e40befcd0e4be425da1d7fb9e94bacc18f8f
Paul PLANCQ <paul.plancq@outlook.fr>
2025-07-07 18:49:42 +0200

    feat(css,react): add Input component with customizable props and styles

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,3 +2,4 @@ export { Grid, type GridProps } from './Grid/Grid';
 export { Typography, type TypographyProps } from './Typography/Typography';
 export { Icon, type IconProps } from './Icon/Icon';
 export { Button, type ButtonProps } from './Button/Button';
+export { Input, type InputProps } from './Input/Input';
