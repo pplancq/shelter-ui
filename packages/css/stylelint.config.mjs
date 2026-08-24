46102b8892e59f78179d5dfedd97c10e8b14a136
Paul PLANCQ <paul.plancq@outlook.fr>
2026-08-24 08:21:48 +0200

    refactor(shelter-ui): update stylelint configuration and ignore rules

--- packages/css/stylelint.config.mjs
+++ packages/css/stylelint.config.mjs
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-default-export
 export default {
-  extends: ["@pplancq/stylelint-config", "@pplancq/stylelint-config/prettier"],
+  extends: ["@pplancq/stylelint-config"],
   rules: {
     // prettier will take care of this
     "at-rule-empty-line-before": null,
